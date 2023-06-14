0726ed69c4c26d8d49be97e2a2e0e2b143f3c796
kabanskiy <yarikkabanskiy@mail.com>
2023-06-14 23:54:48 +0300

    Поставлен header и footer

--- app.py
+++ app.py
@@ -2,9 +2,11 @@ from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
+@app.route('/')
 @app.route('/home')
 def home():
-    return render_template('home.html')
+    return "hello Uassya"
+    # return render_template('home.html')
 
 @app.route('/journeys')
 def journeys():
@@ -14,6 +16,9 @@ def journeys():
 def places():
     return render_template('places.html')
 
+# @app.route('/user/<string:name>/<int:id>')
+# def user(name, id):
+#     return 'user page' + name + "-" + str(id)
 
 if __name__=='__main__':
     app.run(debug=True)
