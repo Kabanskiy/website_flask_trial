f2db1d1540665c0b7036ec04ee2b7cb8adc308e9
kabanskiy <yarikkabanskiy@mail.com>
2023-06-15 00:12:46 +0300

    Поставлен header и footer

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, request
+# from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# db = SQLAlchemy(app)
 @app.route('/')
 @app.route('/home')
 def home():
-    return "hello Uassya"
-    # return render_template('home.html')
+    return render_template('home.html')
 
 @app.route('/journeys')
 def journeys():
