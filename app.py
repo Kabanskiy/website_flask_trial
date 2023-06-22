4a933123a3c56c5aa64e435281a47032965b4822
kabanskiy <yarikkabanskiy@mail.com>
2023-06-22 23:28:52 +0300

    Добавлены посты

--- app.py
+++ app.py
@@ -24,17 +24,22 @@ def __repr__(self):
 def home():
     return render_template('home.html')
 
-@app.route('/journeys')
-def journeys():
-    return render_template('journeys.html')
-
 @app.route('/places')
 def places():
-    fishes = Fish.query.order_by(Fish.data).all()
-    return render_template('places.html', fishes=fishes)
+    return render_template('places.html')
 
-@app.route('/create-place', methods=['POST', 'GET'])
-def create_place():
+@app.route('/journeys')
+def journeys():
+    fishes = Fish.query.order_by(Fish.date.desc()).all()
+    return render_template('journeys.html', fishes=fishes)
+
+@app.route('/journeys/int:id>')
+def journey_read(id):
+    fish = Fish.query.get(id)
+    return render_template('journey_read.html', fish=fish)
+
+@app.route('/create-journey', methods=['POST', 'GET'])
+def create_journey():
     if request.method == 'POST':
         title = request.form['title']
         intro = request.form['intro']
@@ -45,11 +50,11 @@ def create_place():
         try:
             db.session.add(fish)
             db.session.commit()
-            return redirect('/home')
+            return redirect('/journeys')
         except:
             return 'Что-то пошло не так...'
     else:
-        return render_template('create-place.html')
+        return render_template('create-journey.html')
 
 
 if __name__=='__main__':
